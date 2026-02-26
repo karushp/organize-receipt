390373af6e561f3563830b081b21812adf78b957
Karush <115478599+karushp@users.noreply.github.com>
2026-02-26 09:34:15 +0000

    Initial Porject structure

---
+++ config.py
@@ -0,0 +1,23 @@
+USER_CONFIG = {
+    "KP": {
+        "sheet_id": "1a2b3c4d5e6f7g8h9i0j",
+        "drive_folder_id": "0a1b2c3d4e5f6g7h8i9j"
+    },
+    "ASB": {
+        "sheet_id": "9i8h7g6f5e4d32a1b0c",
+        "drive_folder_id": "9j8i7h6f5e4d3c2b1a"
+    }
+}
+
+USERS = [
+    "KP",
+    "ASB"
+]
+
+CATEGORIES = [
+    "Food",
+    "Transportation",
+    "Entertainment",
+    "Utilities",
+    "Shopping"
+]
